233b9e9f087b8d18d69419a373960e98bdea36b2
arnavsaini55 <arnavsaini.saini123@gmail.com>
2025-08-22 10:45:35 +0530

    headet,tasklist props

--- components/Input/Input.js
+++ components/Input/Input.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { View, Text, Pressable } from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faCheck, faFlag, faSquare } from "@fortawesome/free-solid-svg-icons";
+import style from "./style";
 
-const input= () => {
+const Input = ({ title, checklist, priority }) => {
+  const [checked, setChecked] = useState(checklist);
 
-}
+  const getPriorityColor = () => {
+    switch (priority) {
+      case "high":
+        return "red";
+      case "medium":
+        return "orange";
+      case "low":
+        return "green";
+      default:
+        return "grey";
+    }
+  };
 
-export default input
+  return (
+    <View style={style.container}>
+      <FontAwesomeIcon
+        icon={faFlag}
+        size={20}
+        color={getPriorityColor()}
+        style={{ marginRight: 10 }}
+      />
+      <Text style={[style.title, checked && style.done]}>{title}</Text>
+      <Pressable onPress={() => setChecked(!checked)}>
+        <FontAwesomeIcon
+          icon={checked ? faCheck : faSquare}
+          size={20}
+          color={checked ? "green" : "grey"}
+        />
+      </Pressable>
+    </View>
+  );
+};
+
+Input.defaultProps = {
+  title: "",
+  checklist: false,
+  priority: "low",
+};
+
+Input.propTypes = {
+  title: PropTypes.string.isRequired,
+  checklist: PropTypes.bool,
+  priority: PropTypes.oneOf(["low", "medium", "high"]),
+};
+
+export default Input;
--- components/Input/style.js
+++ components/Input/style.js
@@ -1,7 +1,22 @@
 import { StyleSheet } from "react-native";
 
 const style = StyleSheet.create({
-
+ container: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 6,
+    padding: 10,
+    backgroundColor: "#f8f8f8",
+    borderRadius: 8,
+  },
+  title: {
+    flex: 1,
+    fontSize: 16,
+  },
+  done: {
+    textDecorationLine: "line-through",
+    color: "gray",
+  },
 })
 
 
